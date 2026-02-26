bb4a47eda5229356ed078b806475ff664121b53d
Mateus Canjerana <88593542+MateusCanjerana@users.noreply.github.com>
2026-02-26 15:06:28 -0300

    aula35

---
+++ aula35/assets/js/main.js
@@ -0,0 +1,20 @@
+const elementos = [
+    {tag: 'p', texto: 'Qualquer texto'},
+    {tag: 'div', texto: 'Frase 2'},
+    {tag: 'footer', texto: 'Frase 3'},
+    {tag: 'section', texto: 'Frase 4'},
+];
+
+const container =  document.querySelector('.container');
+const div = document.createElement('div');
+
+for(let i = 0; i < elementos.length; i++ ){
+    let {tag, texto} = elementos[i];
+    let tagCriada = document.createElement(tag);
+    tagCriada.innerHTML = texto; //Ou pode-se usar 'innerText' em vez de 'innerHTML' para evitar que o texto seja interpretado como código HTML.
+    let textoCriado = document.createTextNode(texto);
+    tagCriada.appendChild(textoCriado);
+    div.appendChild(tagCriada);
+}
+console.log(div)
+container.appendChild(div);
