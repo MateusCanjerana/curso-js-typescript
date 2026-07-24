9f3dd9f02294942992c470f03d45277fea2ceebe
Mateus Canjerana <88593542+mateuscanjerana@users.noreply.github.com>
2026-07-23 21:10:51 -0300

    update

---
+++ aula36-(for-in)/index.js
@@ -0,0 +1,16 @@
+const frutas = ['Pera', 'Maçã', 'Uva']; 
+//for (let i = 0; i< frutas.length; i++){
+//    console.log(frutas[i]);
+//}
+for (let i in frutas){ //for in é usado para percorrer os índices do array
+    console.log(frutas[i]); //i é o índice do array 
+}
+const pessoa = {
+    nome: 'Mateus',
+    sobrenome: 'Canjerana',
+    idade: 25
+};
+for (let key in pessoa){ //for in é usado para percorrer as chaves do objeto
+    console.log(pessoa.nome);
+    console.log(key, pessoa[key]); //key é a chave do objeto e pessoa[key] é o valor da chave
+}
